5eb1f5a0dfa8e13076285d79e693e5ed775ba15b
Alex Stiles <alinnstiles@gmail.com>
2024-05-09 09:43:54 -0400

    completed lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,115 +1,116 @@
 class Article:
 
-    all = []
-
+    all=[]
+    
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
         Article.all.append(self)
 
-    def __repr__(self) -> str:
-        return f"Article(author = {self.author}, magazine={self.magazine}, title={self.title})"
-
     @property
     def title(self):
         return self._title
-
+        # test this if correct it is
     @title.setter
-    def title(self, value):
-        if type(value) == str and len(value) >= 5 and len(value) <= 50:
-            self._title = value
+    def title(self, title):
+        if hasattr(self, '_title'):
+            print("Title can not be changed")
+        elif type(title) != str:
+            print ("Title must be of type string")
+        elif  len(title) < 5 or len(title) > 50:
+            print("Title must be between 5 and 50 characters")
         else:
-            raise Exception
-        if hasattr(self, 'title') == False:
-            raise Exception
+            self._title =title
+
+
 
 
 class Author:
-    def __init__(self, name):
+    def __init__(self, name:str):
         self.name = name
-
-    def articles(self):
-        return [article for article in Article.all if article.author == self]
-
-    def magazines(self):
-        return list({article.magazine for article in Article.all if article.author == self})
-
-    def add_article(self, magazine, title):
-        return Article(author=self, magazine=magazine, title=title)
-
-    def topic_areas(self):
-        topics_list = list({magazine.category for magazine in self.magazines()})
-        if len(topics_list) > 0:
-            return topics_list
-        else:
-            return None
-
     @property
     def name(self):
         return self._name
-
     @name.setter
-    def name(self, value):
-        if type(value) == str and len(value) > 0:
-            self._name = value
+    def name (self, name):
+        if hasattr(self, "_name"):
+            print ("Name can not be changed")
+        elif type(name) != str:
+            print("Name must be of type string")
+        elif len(name) <= 0:
+            print("Name must be longer than 0 characters")
         else:
-            raise TypeError("Author must be of type string and have more than 0 characters.")
-        if hasattr(self, 'name') == False:
-            raise ValueError("Name cannot be changed after instantiated.")
+            self._name = name
+        pass
+
+    def articles(self):
+        return[article for article in Article.all if article.author == self]
+    
+
+    def magazines(self):
+        return list(set([article.magazine for article in Article.all if article.author == self]))
+
+    def add_article(self, magazine, title):
+        return Article(self, magazine, title)
+
+    def topic_areas(self):
+        top_areas_list = list(set([article.magazine.category for article in Article.all if article.author == self]))
+        if top_areas_list==[]:
+            return None
+        else:
+            return top_areas_list
+        pass
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
 
-    def articles(self):
-        return [article for article in Article.all if article.magazine == self]
-
-    def contributors(self):
-        return list({article.author for article in Article.all if article.magazine == self})
-
-    def article_titles(self):
-        titles_list = list({article.title for article in self.articles()})
-        if len(titles_list) > 0:
-            return titles_list
-        else:
-            return None
-
-    def contributing_authors(self):
-        contributors_list = [article.author for article in Article.all if article.magazine == self]
-        sorted_list = sorted(contributors_list, key=lambda author: author.name)
-        i = 0
-        contributing_authors = []
-        for author in sorted_list:
-            if sorted_list[i] == sorted_list[i + 1]:
-                contributing_authors.append(author)
-
-        if len(contributing_authors) > 0:
-            return contributing_authors
-        else:
-            return None
-
-
-
     @property
     def name(self):
         return self._name
-
     @name.setter
-    def name(self, value):
-        if type(value) == str and len(value) >= 2 and len(value) <= 16:
-            self._name = value
-        else:
-            raise Exception
-
+    def name(self, name):
+        if type(name) == str and 2 <= len(name) <=16:
+            self._name = name
+    
     @property
     def category(self):
         return self._category
-
+    
     @category.setter
-    def category(self, value):
-        if type(value) == str and len(value) > 0:
-            self._category = value
+    def category(self, category):
+        if type(category) == str and 0 < len(category): 
+            self._category = category
+
+
+    def articles(self):
+        return[article for article in Article.all if article.magazine == self]
+        
+    def contributors(self):
+        return list(set([article.author for article in Article.all if article.magazine == self]))
+        
+
+    def article_titles(self):
+        article_title_list = [article.title for article in Article.all if article.magazine == self]
+        if article_title_list==[]:
+            return None
         else:
-            raise Exception
+            return article_title_list
+    def contributing_authors(self):
+        author_list = [article.author for article in Article.all if article.magazine == self]
+        author_counts = {}
+        contributing_authors = []
+
+        for author in author_list:
+            author_counts[author] = author_counts.get(author, 0) + 1
+
+        for author, count in author_counts.items():
+            if count > 2:
+                contributing_authors.append(author)
+
+        if contributing_authors:
+            return contributing_authors
+        else:
+            return None
